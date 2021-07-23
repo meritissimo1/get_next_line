2d589476a8e54f40781954814d9b80c61bfbb779
meritissimo1 <marcelomoreira1905@gmail.com>
2021-07-23 18:35:37 -0300

    adicionando os bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/23 18:34:00 by marcrodr          #+#    #+#             */
+/*   Updated: 2021/07/23 18:34:10 by marcrodr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlen(const char *str);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/23 18:29:01 by marcrodr          #+#    #+#             */
+/*   Updated: 2021/07/23 18:29:15 by marcrodr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*res;
+	size_t	len;
+	size_t	i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	res = malloc(len + 1);
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		res[i++] = *s1++;
+	while (*s2)
+		res[i++] = *s2++;
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dst;
+	int		slen;
+	int		offset;
+
+	slen = ft_strlen(s);
+	dst = (char *)malloc((slen + 1) * sizeof(char));
+	if (!dst)
+		return (0);
+	offset = 0;
+	while (offset < slen + 1)
+	{
+		((unsigned char *)dst)[offset] = ((unsigned char *)s)[offset];
+		offset++;
+	}
+	return (dst);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	char			current_char;
+	unsigned int	i;
+
+	if (src == NULL)
+		return (0);
+	if (size > 0)
+	{
+		i = 0;
+		current_char = src[i];
+		while (current_char != '\0' && i < (size - 1))
+		{
+			dst[i] = current_char;
+			i++;
+			current_char = src[i];
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	s_len;
+
+	if (!s)
+		return (0);
+	if (ft_strlen(s) < start)
+		return (calloc(1, sizeof(char)));
+	s_len = ft_strlen(s + start);
+	if ((s_len) < len)
+		len = s_len;
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, (s + start), len + 1);
+	return (str);
+}
