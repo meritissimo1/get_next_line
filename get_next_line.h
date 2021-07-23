05cd8ec0e2b1a02ae48bd03cef776d445e68a449
meritissimo1 <marcelomoreira1905@gmail.com>
2021-07-23 17:51:42 -0300

    alterando cut_line

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 17:10:08 by marcrodr          #+#    #+#             */
-/*   Updated: 2021/07/22 19:24:33 by marcrodr         ###   ########.fr       */
+/*   Updated: 2021/07/23 15:57:25 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # include <unistd.h>
 # include <string.h>
 
-char *ft_substr(char const *s, unsigned int start, size_t len);
-char *ft_strjoin(const char *s1, const char *s2);
-char *ft_strdup(const char *s1);
-void *ft_memcpy(void *dst, const void *src, size_t n);
-size_t ft_strlen(const char *str);
-char *get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlen(const char *str);
+char	*get_next_line(int fd);
 
 #endif
